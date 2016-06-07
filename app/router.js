d690ede5c128c6f2677d6c587dca6169ce0e3ce1
adam craig and nick arrasate <nick@>
2016-06-07 11:34:37 -0700

    Add linking to individual pages for articles

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('about');
   this.route('story');
   this.route('new-article');
+  this.route('article-view', {path: '/article-view/:article_id'});
 });
 
 export default Router;
